d1593b67bfc97c906e5987a110709fe503b6ee41
Emmanuel Isong <111416145+EmmanuelisongDev@users.noreply.github.com>
2022-08-30 18:09:22 +0100

    Add files via upload

--- 08_Day_Objects/08_day_starter/scripts/main.js
+++ 08_Day_Objects/08_day_starter/scripts/main.js
@@ -272,57 +272,11 @@ const countries2 = [
 
 console.log(`${countries2["name"]},${countries2["capital"]},${countries2["population"]},${countries2["languages"]}`)
 
-//Level 3
-let personAccount = {
-    firstName: "victor",
-    lastName: "kenneth",
-    income: {
-        freelancing: 10000,
-        menialLabour: 2000
-    },
-    expenses: {
-        data: 500,
-        food: 1000,
-        transport: 500
-    },
 
-    totalIncome: function () {
-        let tIncome = 0;
-        let values = Object.values(this.income);
-        for (let i = 0; i < values.length; i++) {
-            tIncome += values[i]
-        }
-        return tIncome;
-    },
-    totalExpenses: function () {
-        let tExpenses = 0;
-        let values = Object.values(this.expenses);
-        for (let i = 0; i < values.length; i++) {
-            tExpenses += values[i]
-        }
-        return tExpenses
-    },
-    accountBalance: function () {
-        return this.totalIncome() - this.totalExpenses();
-    },
-    accountInfo: function () {
-        return `name:${this.firstName} ${this.lastName}\n
-                incomes:${Object.entries(this.income)}
-                expenses: ${
-                    Object.entries(this.expenses)
-                }
-                totalIncome: ${this.totalIncome()}
-                totalExpenses: ${this.totalExpenses()}
-                accountBalance: ${this.accountBalance()}
-                `
-    }
-}
 
-function callBack(add, a, b) {
-    return add(a, b);
-}
 
-//user
+// //Level 3
+
 const users = [{
         _id: 'ab12ex',
         username: 'Alex',
@@ -365,6 +319,8 @@ const users = [{
     }
 ];
 
+
+
 const signUp = (username, email, password) => {
     users.forEach(user => {
         if (user.username === username && user.password === password) {
@@ -398,10 +354,10 @@ const signIn = (username, password) => {
             return "wrong username or password";
         }
     }
-    // console.log(foundUser)
+     console.log(foundUser)
 }
 
-//product
+// //product
 const products = [{
         _id: 'eedfcf',
         name: 'mobile phone',
